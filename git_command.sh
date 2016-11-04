dd4fc98772f8dda07dec75e86e778f576baddbf5
tatum su <tianbin@yit.com>
2016-11-04 15:44:20 +0800

    Add remote branch command

--- git_command.sh
+++ git_command.sh
@@ -4,3 +4,11 @@ git init
 git add * .
 git commit -m "refine"
 git push
+
+git remote add git_study git@github.com:tatumsu/git_study.git
+git push -u git_study master
+git remote show git_study
+
+git tag -a 0.1 -m "version 0.1"
+git tag -l
+git show 0.1
